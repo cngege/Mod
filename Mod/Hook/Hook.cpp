fec21b71e781512bb6f62d4a1ba58930ad4b9f0f
muzili <2586850402@qq.com>
2022-08-08 11:42:54 +0800

    优化Hook 时的错误日志输出

--- Mod/Hook/Hook.cpp
+++ Mod/Hook/Hook.cpp
@@ -40,47 +40,63 @@ auto Hook::init() -> void
 	logF("Hook::init is start runner……");
 
 	playerkb = FindSignature("8B 02 89 81 ? ? 00 00 8B 42 04 89 81 ? ? 00 00 8B 42 08 89 81 ? ? 00 00 C3");
-	Player::SpeedXOffset = *reinterpret_cast<int*>(playerkb + 4);
-	Player::SpeedYOffset = *reinterpret_cast<int*>(playerkb + 13);
-	Player::SpeedZOffset = *reinterpret_cast<int*>(playerkb + 22);
-	MH_CreateHookEx((LPVOID)playerkb, &Hook::PlayerKB, &playercall);
-	if (MH_EnableHook((LPVOID)playerkb) != MH_OK) {
-		logF("[Hook error] [%s] is no found Hook point","playerkb");
+	if (playerkb != 0x00) {
+		Player::SpeedXOffset = *reinterpret_cast<int*>(playerkb + 4);
+		Player::SpeedYOffset = *reinterpret_cast<int*>(playerkb + 13);
+		Player::SpeedZOffset = *reinterpret_cast<int*>(playerkb + 22);
+		MH_CreateHookEx((LPVOID)playerkb, &Hook::PlayerKB, &playercall);
+		MH_EnableHook((LPVOID)playerkb);
+	}else {
+		logF("[Hook error] [%s] is no found Hook point", "playerkb");
 	}
 
 	//clientInstance::Tick
 	clientInstanceTick = FindSignature("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 48 8B F9 48 8B 01");
-	MH_CreateHookEx((LPVOID)clientInstanceTick, &Hook::ClientInstance_Tick, &clientInstance_Tickcall);
-	if (MH_EnableHook((LPVOID)clientInstanceTick) != MH_OK) {
+	if (clientInstanceTick != 0x00) {
+		MH_CreateHookEx((LPVOID)clientInstanceTick, &Hook::ClientInstance_Tick, &clientInstance_Tickcall);
+		MH_EnableHook((LPVOID)clientInstanceTick);
+	}
+	else {
 		logF("[Hook error] [%s] is no found Hook point", "clientInstanceTick");
 	}
 
+
 	//是否显示坐标 Tick
 	is_ShowCoordinatesTick = FindSignature("48 83 EC ? 48 8B 49 ? 48 8B 01 FF 90 ? ? ? ? 48 85 C0 74 ? 48 8B 88");
-	MH_CreateHookEx((LPVOID)is_ShowCoordinatesTick, &Hook::Is_ShowCoordinates_Tick, &is_ShowCoordinates_Tickcall);
-	if (MH_EnableHook((LPVOID)is_ShowCoordinatesTick) != MH_OK) {
+	if (is_ShowCoordinatesTick != 0x00) {
+		MH_CreateHookEx((LPVOID)is_ShowCoordinatesTick, &Hook::Is_ShowCoordinates_Tick, &is_ShowCoordinates_Tickcall);
+		MH_EnableHook((LPVOID)is_ShowCoordinatesTick);
+	}
+	else {
 		logF("[Hook error] [%s] is no found Hook point", "is_ShowCoordinatesTick");
 	}
 
 	//获取饥饿值的地址的函数  实际上被用来做修改玩家速度
 	getHungerValAddressTick = FindSignature("4C 8B D1 44 0F B6 CA 49 BB ? ? ? ? ? ? ? ? 48 B8 ? ? ? ? ? ? ? ? 4C 33 C8 8B C2 4D 0F AF CB C1 E8 08 44 0F B6 C0 8B C2 4D 33 C8 C1 E8 10 4D 8B 42 08 4D 0F AF CB 0F B6 C8 4C 33 C9 8B C2 49 8B 4A 30 4D 0F AF CB 48 C1 E8 18 4C 33 C8 4D 0F AF CB 49 23 C9 48 C1 E1 04 49 03 4A 18 48 8B 41 08 49 3B C0 74 27 48 8B 09 3B 50 10 74 0E 48 3B C1 74 1A 48 8B 40 08 3B 50 10 75 F2 48 85 C0 49 0F 44 C0 49 3B C0 74 05 ? ? ? ? C3 48 8D 05 ? ? ? ? C3");
-	MH_CreateHookEx((LPVOID)getHungerValAddressTick, &Hook::GetHungerValAddress_Tick, &getHungerValAddress_Tickcall);
-	if (MH_EnableHook((LPVOID)getHungerValAddressTick) != MH_OK) {
+	if (getHungerValAddressTick != 0x00) {
+		MH_CreateHookEx((LPVOID)getHungerValAddressTick, &Hook::GetHungerValAddress_Tick, &getHungerValAddress_Tickcall);
+		MH_EnableHook((LPVOID)getHungerValAddressTick);
+	}
+	else {
 		logF("[Hook error] [%s] is no found Hook point", "getHungerValAddressTick");
 	}
 
 
 	//获取能够加上偏移得出破坏进度的地址(第一个参数) 可用来快速破坏方块
 	destroyBlockingAddress = FindSignature("48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 56 48 83 EC 30 48 8B D9 0F 29 74 24 ? 48 8B 49 08");
-	MH_CreateHookEx((LPVOID)destroyBlockingAddress, &Hook::DestroyBlocking, &destroyBlockingAddresscall);
-	if (MH_EnableHook((LPVOID)destroyBlockingAddress) != MH_OK) {
+	if (destroyBlockingAddress != 0x00) {
+		MH_CreateHookEx((LPVOID)destroyBlockingAddress, &Hook::DestroyBlocking, &destroyBlockingAddresscall);
+		MH_EnableHook((LPVOID)destroyBlockingAddress);
+	}else {
 		logF("[Hook error] [%s] is no found Hook point", "destroyBlockingAddress");
 	}
 
 	//掉落无伤 仅本地有效
 	noFallDamage_tick = FindSignature("48 89 5C 24 ? 57 48 83 EC 40 48 8B D9 48 8B FA 48 8B 89 ? ? ? ? 48 8B 01");
-	MH_CreateHookEx((LPVOID)noFallDamage_tick, &Hook::NoFallDamage_Tick, &noFallDamage_Tickcall);
-	if (MH_EnableHook((LPVOID)noFallDamage_tick) != MH_OK) {
+	if (noFallDamage_tick != 0x00) {
+		MH_CreateHookEx((LPVOID)noFallDamage_tick, &Hook::NoFallDamage_Tick, &noFallDamage_Tickcall);
+		MH_EnableHook((LPVOID)noFallDamage_tick);
+	}else {
 		logF("[Hook error] [%s] is no found Hook point", "noFallDamage_tick");
 	}
 
