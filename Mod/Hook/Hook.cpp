fa24dea928ae8e376c390d9c6686cc51b5ed0437
muzili <2586850402@qq.com>
2022-08-10 15:21:27 +0800

    再次调整抗击退部分

--- Mod/Hook/Hook.cpp
+++ Mod/Hook/Hook.cpp
@@ -157,13 +157,12 @@ auto Hook::exit() -> void {
 auto Hook::PlayerKB(Player* player,vec3_t* kb) -> void
 {
 	//长按鼠标右键
-	if (!KEY_DOWN(VK_RBUTTON)) {
+	if (KEY_DOWN(VK_RBUTTON)) {
 		kb->x *= 0.1f;
 		kb->y *= 0.1f;
 		kb->z *= 0.1f;
-		playercall(player, kb);
-		return;
 	}
+	playercall(player, kb);
 	/*
 	auto p = (Player*)(instance->LocalPlayer());
 	if(p != nullptr)
