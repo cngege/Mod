676b029c57ed6ff739bbc584c8971d5b804dc0fd
muzili <2586850402@qq.com>
2022-08-16 23:49:17 +0800

    GameMode虚表的函数定义

--- SDK/GameMode.cpp
+++ SDK/GameMode.cpp
@@ -1,1 +1,8 @@
 #include "GameMode.h"
+
+
+uintptr_t** GameMode::vTables = nullptr;
+
+auto GameMode::SetVtables(uintptr_t** vTable)->void {
+	vTables = vTable;
+}
--- SDK/GameMode.h
+++ SDK/GameMode.h
@@ -1,5 +1,14 @@
 #pragma once
+#include "../Mod/Utils/Utils.h"
+
 
 class GameMode {
+private:
+	static uintptr_t** vTables;
+public:
+	static auto SetVtables(uintptr_t** vTables)->void;
+
+	// Ðé±íº¯Êý
+public:
 
 };
