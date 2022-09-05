ac83065ac40ad7a1064b899873b89b94e61e7d11
muzili <2586850402@qq.com>
2022-09-05 13:00:58 +0800

    所有模块事件 现在统一由模块自己内部判断模块是否开启

---
+++ Mod/Modules/Modules/NoAttackFriend.cpp
@@ -0,0 +1,26 @@
+﻿#include "NoAttackFriend.h"
+#include "Actor.h"
+#include "LocalPlayer.h"
+#include "../../Utils/Game.h"
+
+NoAttackFriend::NoAttackFriend() : Module(VK_F4, "NoAttackFriend", "从名称颜色识别玩家为队友时拦截攻击") {
+
+}
+
+auto NoAttackFriend::onAttack(Actor* actor)->bool {
+	if (!isEnabled() || !actor->isPlayerEx()) {
+		return true;
+	}
+	auto name = actor->getNameTag();
+	auto myname = Game::localplayer->getNameTag();
+	if ((char)myname == colorbyte[0] && (char)name == colorbyte[0]) {		//判断自己和对方名字是否是带有颜色 即§开头
+		if ((char)(myname + 1) == (char)(name + 1)) {						//判断（如果是）颜色（那）是否是一样
+			for (int i = 1; i < 16; i++) {									//判断符号后面是否接颜色，长度是定值
+				if (colorbyte[i] == (char)(myname + 1)) {
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
---
+++ Mod/Modules/Modules/NoAttackFriend.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "../Module.h"
+
+
+class NoAttackFriend : public Module
+{
+public:
+	NoAttackFriend();
+
+public:
+	char colorbyte[17] = {(char)'§',(char)'0',(char)'1',(char)'2',(char)'3',(char)'4',(char)'5',(char)'6',(char)'7',(char)'8',(char)'9',(char)'a',(char)'b',(char)'c',(char)'d',(char)'e',(char)'f'};
+
+public:
+	virtual auto onAttack(class Actor*)->bool override;
+};
