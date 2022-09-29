cd2cf3983d6871e2958c5e0328b3d6d9b89935cd
muzili <2586850402@qq.com>
2022-09-29 15:47:10 +0800

    FastViewPerspective模块 按下快捷键时锁定玩家移动方向

--- Mod/Modules/Modules/FastViewPerspective.h
+++ Mod/Modules/Modules/FastViewPerspective.h
@@ -4,7 +4,12 @@
 class FastViewPerspective : public Module {
 public:
 	FastViewPerspective();
-
+private:
+	float lpRotVertical = -30.0f;
 public:
 	auto isToggle()->bool;
+	
+public:
+	virtual auto getBindKeyName()->std::string override;
+	virtual auto onTick(class GameMode* gm)->void override;
 };
