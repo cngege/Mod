8d845c92e82521590d7c90484733c0aec80389b6
muzili <2586850402@qq.com>
2023-08-21 00:26:55 +0800

    按下热键时对人称修改后的人称视角 可以用UI设定

--- Mod/Modules/Modules/FastViewPerspective.cpp
+++ Mod/Modules/Modules/FastViewPerspective.cpp
@@ -2,6 +2,8 @@
 #include "../../Utils/Game.h"
 #include "../../Utils/Utils.h"
 
+#include "imgui.h"
+
 #include "GameMode.h"
 #include "LocalPlayer.h"
 
@@ -17,15 +19,36 @@ auto FastViewPerspective::getBindKeyName()->std::string {
 	return name;
 }
 
+auto FastViewPerspective::onInternalImGUIRender() -> void
+{
+	ImGui::Text("选择该功能触发时展示的人称视角");
+	ImGui::RadioButton("第一人称", &ViewPerspective, 0);
+	ImGui::SameLine();
+	ImGui::RadioButton("第二人称", &ViewPerspective, 1);
+	ImGui::SameLine();
+	ImGui::RadioButton("第三人称", &ViewPerspective, 2);
+
+}
+
 
 auto FastViewPerspective::isToggle()->bool {
 	return Game::IsMouseDown(VK_XBUTTON1);
 	//return Game::IsKeyDown('R');
 }
 
+auto FastViewPerspective::getViewPerspective(int source) -> int
+{
+	if (isToggle()) {
+		return ViewPerspective;
+	}
+	return source;
+}
+
 auto FastViewPerspective::onloadConfigFile(json& data)->void {
 	setEnabled(config::readDataFromJson<bool>(data, "enable", true));
+	ViewPerspective = config::readDataFromJson<int>(data, "ViewPerspective", 2);
 }
 auto FastViewPerspective::onsaveConfigFile(json& data)->void {
 	data["enable"] = isEnabled();
+	data["ViewPerspective"] = ViewPerspective;
 }
--- Mod/Modules/Modules/FastViewPerspective.h
+++ Mod/Modules/Modules/FastViewPerspective.h
@@ -1,14 +1,20 @@
-#pragma once
+﻿#pragma once
 #include "../Module.h"
 
 class FastViewPerspective : public Module {
 public:
 	FastViewPerspective();
+
+private:
+	int ViewPerspective = 2;
+
 public:
 	auto isToggle()->bool;
+	auto getViewPerspective(int source) -> int;
 	
 public:
 	virtual auto getBindKeyName()->std::string override;
+	virtual auto onInternalImGUIRender() -> void override;
 	virtual auto onloadConfigFile(json& data)->void override;
 	virtual auto onsaveConfigFile(json& data)->void override;
 };
