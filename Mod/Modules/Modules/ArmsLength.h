61bd534b41c6691884da6969caf52cddf59df5b5
muzili <2586850402@qq.com>
2023-08-21 00:25:53 +0800

    对两个变量（指针）做初始化赋值

--- Mod/Modules/Modules/ArmsLength.h
+++ Mod/Modules/Modules/ArmsLength.h
@@ -10,8 +10,8 @@ private:
 	//float distance = 7;
 
 
-	BYTE sJumpaddr;							// 未作修改是jmp地址的值，以用作还原
-	BYTE* jumpaddr;
+	BYTE sJumpaddr = 0;							// 未作修改是jmp地址的值，以用作还原
+	BYTE* jumpaddr = nullptr;
 public:
 	virtual auto onEnable()->void override;
 	virtual auto onDisable()->void override;
