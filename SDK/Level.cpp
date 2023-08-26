c432401bce9e8986daa1a25aa94f0fa1beaa6ff6
muzili <2586850402@qq.com>
2023-08-27 00:04:56 +0800

    使用 Utils::CallVFunc 实现虚表函数的调用。 添加 Level::getSeed 方法

--- SDK/Level.cpp
+++ SDK/Level.cpp
@@ -1,4 +1,5 @@
 ﻿#include "Level.h"
+#include "Utils.h"
 
 uintptr_t* Level::startLeaveGameCall = nullptr;
 //uintptr_t* Level::forEachPlayerCall = nullptr;
@@ -21,11 +22,19 @@ auto Level::getAllPlayer()->std::vector<Player*> {
 
 
 auto Level::getTime()->int {
-	return reinterpret_cast<int(__fastcall*)(Level*)>((*(uintptr_t**)this)[112])(this);
+	return Utils::CallVFunc<112, int>(this);
+	//return reinterpret_cast<int(__fastcall*)(Level*)>((*(uintptr_t**)this)[112])(this);
 }
 
 auto Level::setTime(int time)->void {
-	return reinterpret_cast<void(__fastcall*)(Level*,int)>((*(uintptr_t**)this)[113])(this,time);
+	return Utils::CallVFunc<113, void,int>(this, time);
+	//return reinterpret_cast<void(__fastcall*)(Level*,int)>((*(uintptr_t**)this)[113])(this,time);
+}
+
+auto Level::getSeed() -> unsigned int
+{
+	return Utils::CallVFunc<114, unsigned int>(this);
+	//return reinterpret_cast<unsigned int(__fastcall*)(Level*)>((*(uintptr_t**)this)[114])(this);
 }
 
 // 虚表Hook
@@ -46,5 +55,23 @@ auto Level::Tick() -> void
 // 208 Level::forEachPlayer(class std::function<bool __cdecl(class Player const & __ptr64)>)
 // 虚表 检查版本 1.20
 auto Level::forEachPlayer(std::function<bool(class Player&)> fp)->void {
-	return reinterpret_cast<void(__fastcall*)(Level*, std::function<bool(class Player&)>)>((*(uintptr_t**)this)[207])(this, fp);
+	return Utils::CallVFunc<207, void, std::function<bool(class Player&)>>(this, fp);
+	//return reinterpret_cast<void(__fastcall*)(Level*, std::function<bool(class Player&)>)>((*(uintptr_t**)this)[207])(this, fp);
+}
+
+// 虚表 检查版本 1.20.15
+auto Level::isClientSide() -> bool
+{
+	return Utils::CallVFunc<291, bool>(this);
+	//return reinterpret_cast<bool(__fastcall*)(Level*)>((*(uintptr_t**)this)[291])(this);
+}
+
+auto Level::setSimPaused(bool v) -> void
+{
+	return Utils::CallVFunc<322, void, bool>(this, v);
+}
+
+auto Level::getSimPaused() -> bool
+{
+	return Utils::CallVFunc<323, bool>(this);
 }
--- SDK/Level.h
+++ SDK/Level.h
@@ -12,12 +12,16 @@ public:
 	auto getAllPlayer()->std::vector<class Player*>;
 public:
 	//虚表函数
-	auto getTime()->int;															/*111*/
-	auto setTime(int)->void;														/*112*/
+	auto getTime()->int;															/*112*/
+	auto setTime(int)->void;														/*113*/
+	auto getSeed() -> unsigned int;													/*114*/
 	
 	//虚表函数Hook
 	auto startLeaveGame() -> void;													/*2*/
 	auto Tick()->void;																/*101*/
 	// 虚表函数
 	auto forEachPlayer(std::function<bool(class Player&)>) -> void;					/*207-208 /-562(不对)- 用特征码定位call非虚表 */
+	auto isClientSide() -> bool;													/*291*/  // 1.20.15
+	auto setSimPaused(bool) -> void;												/*322*/	 // 1.20.15
+	auto getSimPaused() -> bool;													/*323*/	 // 1.20.15
 };
