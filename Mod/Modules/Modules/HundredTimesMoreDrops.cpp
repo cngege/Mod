663072d7333f7c6422201d3b0668053087d631a7
muzili <2586850402@qq.com>
2023-09-20 18:53:20 +0800

    HundredTimesMoreDrops 掉落倍数在配置中存储 multiple

--- Mod/Modules/Modules/HundredTimesMoreDrops.cpp
+++ Mod/Modules/Modules/HundredTimesMoreDrops.cpp
@@ -3,3 +3,11 @@
 HundredTimesMoreDrops::HundredTimesMoreDrops() : Module(0, "HundredTimesMoreDrops", "百倍掉落物品") {
 	AddIntUIValue("掉落倍数", &multiple, 1, 200);
 }
+
+
+auto HundredTimesMoreDrops::onloadConfigFile(json& data)->void {
+	setEnabled(config::readDataFromJson<int>(data, "multiple", 100));
+}
+auto HundredTimesMoreDrops::onsaveConfigFile(json& data)->void {
+	data["multiple"] = multiple;
+}
--- Mod/Modules/Modules/HundredTimesMoreDrops.h
+++ Mod/Modules/Modules/HundredTimesMoreDrops.h
@@ -7,4 +7,7 @@ public:
 
 public:
 	int multiple = 100;
+public:
+	virtual auto onloadConfigFile(json& data) -> void override;
+	virtual auto onsaveConfigFile(json& data) -> void override;
 };
