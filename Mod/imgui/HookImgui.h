277eca7ad2f5846f898057987ee3d73536451a6f
muzili <2586850402@qq.com>
2023-10-01 20:23:30 +0800

    因为修复了UI崩溃问题，所以之前用于兼容的代码便不必要了 副字体大小+1， 字体文件是否存在做判断

--- Mod/imgui/HookImgui.h
+++ Mod/imgui/HookImgui.h
@@ -101,15 +101,6 @@ HRESULT __fastcall hookPresentD3D12(IDXGISwapChain3* ppSwapChain, UINT syncInter
 	static auto window = (HWND)FindWindowA(nullptr, (LPCSTR)"Minecraft");
 	static auto childwindow = (HWND)FindWindowExA(window, NULL, NULL, (LPCSTR)"Minecraft");
 
-	static auto debugModule = Game::GetModuleManager()->GetModule<Debug*>();
-	static auto render = Game::GetModuleManager()->GetModule<Render*>();
-
-	if (debugModule && debugModule->isEnabled() && debugModule->SynchronousD3D12Render_ImGui) {
-		if (render && !render->isEnabled()) {
-			goto out;
-		}
-	}
-
 	if (window == NULL) {
 		goto out;
 	};
@@ -180,144 +171,10 @@ HRESULT __fastcall hookPresentD3D12(IDXGISwapChain3* ppSwapChain, UINT syncInter
 		mainRenderTargetView->Release();
 		d3d11Device->Release();
 	}
-	else if (deviceType == ID3D_Device_Type::D3D12 && 0) {
-		if (!initContext)
-			ImGui::CreateContext();
-		DXGI_SWAP_CHAIN_DESC sdesc;
-		ppSwapChain->GetDesc(&sdesc);
-		sdesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-		sdesc.OutputWindow = window;
-		sdesc.Windowed = ((GetWindowLongPtr(window, GWL_STYLE) & WS_POPUP) != 0) ? false : true;
-		buffersCounts = sdesc.BufferCount;
-		frameContext = new FrameContext[buffersCounts];
-		D3D12_DESCRIPTOR_HEAP_DESC descriptorImGuiRender = {};
-		descriptorImGuiRender.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
-		descriptorImGuiRender.NumDescriptors = buffersCounts;
-		descriptorImGuiRender.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
-		if (d3d12DescriptorHeapImGuiRender == nullptr)
-			if (FAILED(d3d12Device->CreateDescriptorHeap(&descriptorImGuiRender, IID_PPV_ARGS(&d3d12DescriptorHeapImGuiRender)))) {
-				delete[] frameContext;
-				goto out;
-			}
-		if (d3d12Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&allocator)) != S_OK) {
-			delete[] frameContext;
-			return NULL;
-		}
-		for (size_t i = 0; i < buffersCounts; i++) {
-			frameContext[i].commandAllocator = allocator;
-		};
-		if (d3d12Device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, allocator, NULL, IID_PPV_ARGS(&d3d12CommandList)) != S_OK ||
-			d3d12CommandList->Close() != S_OK) {
-			delete[] frameContext;
-			return NULL;
-		}
-		D3D12_DESCRIPTOR_HEAP_DESC descriptorBackBuffers{};
-		descriptorBackBuffers.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-		descriptorBackBuffers.NumDescriptors = buffersCounts;
-		descriptorBackBuffers.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-		descriptorBackBuffers.NodeMask = 1;
-		if (d3d12Device->CreateDescriptorHeap(&descriptorBackBuffers, IID_PPV_ARGS(&d3d12DescriptorHeapBackBuffers)) != S_OK) {
-			delete[] frameContext;
-			return NULL;
-		}
-		const auto rtvDescriptorSize = d3d12Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
-		rtvHandle = d3d12DescriptorHeapBackBuffers->GetCPUDescriptorHandleForHeapStart();
-		for (unsigned int i = 0; i < buffersCounts; i++) {
-			ID3D12Resource* pBackBuffer = nullptr;
-			frameContext[i].main_render_target_descriptor = rtvHandle;
-			ppSwapChain->GetBuffer(i, IID_PPV_ARGS(&pBackBuffer));
-			d3d12Device->CreateRenderTargetView(pBackBuffer, nullptr, rtvHandle);
-			frameContext[i].main_render_target_resource = pBackBuffer;
-			rtvHandle.ptr += rtvDescriptorSize;
-			pBackBuffer->Release();
-		};
-		if (!initContext) {
-			ImGui_ImplWin32_Init(window);
-			ImGui_ImplDX12_Init(d3d12Device, buffersCounts,
-				DXGI_FORMAT_R8G8B8A8_UNORM,
-				d3d12DescriptorHeapImGuiRender,
-				d3d12DescriptorHeapImGuiRender->GetCPUDescriptorHandleForHeapStart(),
-				d3d12DescriptorHeapImGuiRender->GetGPUDescriptorHandleForHeapStart());
-
-			ImGuiIO& io = ImGui::GetIO();
-			(void)io;
-
-			std::string font_JNMYT = Utils::WStringToString(Logger::GetRoamingFolderPath()) + std::string("\\Mod\\Assets\\JNMYT.ttf");
-			if (_access(font_JNMYT.c_str(), 0 /*F_OK*/) != -1) {
-				io.Fonts->AddFontFromFileTTF(font_JNMYT.c_str(), 15.f, NULL, io.Fonts->GetGlyphRangesChineseFull());
-			}
-			io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\msyh.ttc", 15.f, NULL, io.Fonts->GetGlyphRangesChineseFull());
-			std::wstring mcfolderPath = Utils::getMCFolderPath();
-			io.Fonts->AddFontFromFileTTF((Utils::WStringToString(mcfolderPath) + std::string("\\data\\fonts\\Mojangles.ttf")).c_str(), 15.f, NULL, io.Fonts->GetGlyphRangesChineseFull());
-
-			// 这里注意值如果不是常亮就要当心其被释放掉
-			io.IniFilename = Game::ImConfigIni.c_str();
-
-			initContext = true;
-		};
-		if (d3d12CommandQueue == nullptr) {
-			delete[] frameContext;
-			goto out;
-		}
-		ImGui_ImplDX12_NewFrame();
-		ImGui_ImplWin32_NewFrame();
-		ImGui::NewFrame();
-
-
-		Game::GetModuleManager()->onImGUIRender();
-		{
-			ImGuiStyle* style = &ImGui::GetStyle();
-
-			style->WindowPadding = ImVec2(15, 15);
-			style->WindowRounding = 10.f;
-			style->FramePadding = ImVec2(5, 5);
-			style->FrameRounding = 6.f;
-			style->ItemSpacing = ImVec2(12, 8);
-			style->ItemInnerSpacing = ImVec2(8, 6);
-			style->IndentSpacing = 20.0f;
-			style->ScrollbarSize = 10.0f;
-			style->ScrollbarRounding = 9.0f;
-			style->GrabMinSize = 5.0f;
-			style->GrabRounding = 3.0f;
-			style->WindowTitleAlign = ImVec2(0.5, 0.5);
-		}
-
-
-		FrameContext& currentFrameContext = frameContext[ppSwapChain->GetCurrentBackBufferIndex()];
-		currentFrameContext.commandAllocator->Reset();
-		D3D12_RESOURCE_BARRIER barrier{};
-		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-		barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
-		barrier.Transition.pResource = currentFrameContext.main_render_target_resource;
-		barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
-		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
-		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
-		d3d12CommandList->Reset(currentFrameContext.commandAllocator, nullptr);
-		d3d12CommandList->ResourceBarrier(1, &barrier);
-		d3d12CommandList->OMSetRenderTargets(1, &currentFrameContext.main_render_target_descriptor, FALSE, nullptr);
-		d3d12CommandList->SetDescriptorHeaps(1, &d3d12DescriptorHeapImGuiRender);
-		ImGui::EndFrame();
-		ImGui::Render();
-		ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), d3d12CommandList);
-		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
-		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
-		d3d12CommandList->ResourceBarrier(1, &barrier);
-		d3d12CommandList->Close();
-		d3d12CommandQueue->ExecuteCommandLists(1, reinterpret_cast<ID3D12CommandList* const*>(&d3d12CommandList));
-		d3d12DescriptorHeapBackBuffers->Release();
-		d3d12CommandList->Release();
-		allocator->Release();
-		currentFrameContext.main_render_target_resource->Release();
-		currentFrameContext.commandAllocator->Release();
-		d3d12Device->Release();
-		delete[] frameContext;
-	}
 	else if (deviceType == ID3D_Device_Type::D3D12) {
 		if (d3d12CommandQueue == nullptr) {
 			goto out;
 		}
-
-
 		if (!initContext) {
 			ImGui::CreateContext();
 			
@@ -391,7 +248,9 @@ HRESULT __fastcall hookPresentD3D12(IDXGISwapChain3* ppSwapChain, UINT syncInter
 				if (_access(font_JNMYT.c_str(), 0 /*F_OK*/) != -1) {
 					io.Fonts->AddFontFromFileTTF(font_JNMYT.c_str(), 15.f, NULL, io.Fonts->GetGlyphRangesChineseFull());
 				}
-				io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\msyh.ttc", 15.f, NULL, io.Fonts->GetGlyphRangesChineseFull());
+				if (_access("C:\\Windows\\Fonts\\msyh.ttc", 0 /*F_OK*/) != -1) {
+					io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\msyh.ttc", 16.f, NULL, io.Fonts->GetGlyphRangesChineseFull());
+				}
 				std::wstring mcfolderPath = Utils::getMCFolderPath();
 				io.Fonts->AddFontFromFileTTF((Utils::WStringToString(mcfolderPath) + std::string("\\data\\fonts\\Mojangles.ttf")).c_str(), 15.f, NULL, io.Fonts->GetGlyphRangesChineseFull());
 				initImGuiFont = true;
