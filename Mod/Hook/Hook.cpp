9b9048dd7517b76728700b1c4c3238b8f66754c8
muzili <2586850402@qq.com>
2023-10-09 01:44:02 +0800

    修复更新ImGui后无法对输入框粘贴的问题

--- Mod/Hook/Hook.cpp
+++ Mod/Hook/Hook.cpp
@@ -773,6 +773,132 @@ auto Hook::AllActor_Tick(Actor* _this, float* a1, float a2)->float* {
 //	return actor_moveBBscall(_this, v3);
 //}
 
+ImGuiKey ImGui_VirtualKeyToImGuiKey(WPARAM wParam)
+{
+	switch (wParam)
+	{
+	case VK_TAB: return ImGuiKey_Tab;
+	case VK_LEFT: return ImGuiKey_LeftArrow;
+	case VK_RIGHT: return ImGuiKey_RightArrow;
+	case VK_UP: return ImGuiKey_UpArrow;
+	case VK_DOWN: return ImGuiKey_DownArrow;
+	case VK_PRIOR: return ImGuiKey_PageUp;
+	case VK_NEXT: return ImGuiKey_PageDown;
+	case VK_HOME: return ImGuiKey_Home;
+	case VK_END: return ImGuiKey_End;
+	case VK_INSERT: return ImGuiKey_Insert;
+	case VK_DELETE: return ImGuiKey_Delete;
+	case VK_BACK: return ImGuiKey_Backspace;
+	case VK_SPACE: return ImGuiKey_Space;
+	case VK_RETURN: return ImGuiKey_Enter;
+	case VK_ESCAPE: return ImGuiKey_Escape;
+	case VK_OEM_7: return ImGuiKey_Apostrophe;
+	case VK_OEM_COMMA: return ImGuiKey_Comma;
+	case VK_OEM_MINUS: return ImGuiKey_Minus;
+	case VK_OEM_PERIOD: return ImGuiKey_Period;
+	case VK_OEM_2: return ImGuiKey_Slash;
+	case VK_OEM_1: return ImGuiKey_Semicolon;
+	case VK_OEM_PLUS: return ImGuiKey_Equal;
+	case VK_OEM_4: return ImGuiKey_LeftBracket;
+	case VK_OEM_5: return ImGuiKey_Backslash;
+	case VK_OEM_6: return ImGuiKey_RightBracket;
+	case VK_OEM_3: return ImGuiKey_GraveAccent;
+	case VK_CAPITAL: return ImGuiKey_CapsLock;
+	case VK_SCROLL: return ImGuiKey_ScrollLock;
+	case VK_NUMLOCK: return ImGuiKey_NumLock;
+	case VK_SNAPSHOT: return ImGuiKey_PrintScreen;
+	case VK_PAUSE: return ImGuiKey_Pause;
+	case VK_NUMPAD0: return ImGuiKey_Keypad0;
+	case VK_NUMPAD1: return ImGuiKey_Keypad1;
+	case VK_NUMPAD2: return ImGuiKey_Keypad2;
+	case VK_NUMPAD3: return ImGuiKey_Keypad3;
+	case VK_NUMPAD4: return ImGuiKey_Keypad4;
+	case VK_NUMPAD5: return ImGuiKey_Keypad5;
+	case VK_NUMPAD6: return ImGuiKey_Keypad6;
+	case VK_NUMPAD7: return ImGuiKey_Keypad7;
+	case VK_NUMPAD8: return ImGuiKey_Keypad8;
+	case VK_NUMPAD9: return ImGuiKey_Keypad9;
+	case VK_DECIMAL: return ImGuiKey_KeypadDecimal;
+	case VK_DIVIDE: return ImGuiKey_KeypadDivide;
+	case VK_MULTIPLY: return ImGuiKey_KeypadMultiply;
+	case VK_SUBTRACT: return ImGuiKey_KeypadSubtract;
+	case VK_ADD: return ImGuiKey_KeypadAdd;
+	case (VK_RETURN + 256): return ImGuiKey_KeypadEnter;
+	case VK_LSHIFT: return ImGuiKey_LeftShift;
+	case VK_LCONTROL: return ImGuiKey_LeftCtrl;
+	case VK_LMENU: return ImGuiKey_LeftAlt;
+	case VK_LWIN: return ImGuiKey_LeftSuper;
+	case VK_RSHIFT: return ImGuiKey_RightShift;
+	case VK_RCONTROL: return ImGuiKey_RightCtrl;
+	case VK_RMENU: return ImGuiKey_RightAlt;
+	case VK_RWIN: return ImGuiKey_RightSuper;
+	case VK_APPS: return ImGuiKey_Menu;
+	case '0': return ImGuiKey_0;
+	case '1': return ImGuiKey_1;
+	case '2': return ImGuiKey_2;
+	case '3': return ImGuiKey_3;
+	case '4': return ImGuiKey_4;
+	case '5': return ImGuiKey_5;
+	case '6': return ImGuiKey_6;
+	case '7': return ImGuiKey_7;
+	case '8': return ImGuiKey_8;
+	case '9': return ImGuiKey_9;
+	case 'A': return ImGuiKey_A;
+	case 'B': return ImGuiKey_B;
+	case 'C': return ImGuiKey_C;
+	case 'D': return ImGuiKey_D;
+	case 'E': return ImGuiKey_E;
+	case 'F': return ImGuiKey_F;
+	case 'G': return ImGuiKey_G;
+	case 'H': return ImGuiKey_H;
+	case 'I': return ImGuiKey_I;
+	case 'J': return ImGuiKey_J;
+	case 'K': return ImGuiKey_K;
+	case 'L': return ImGuiKey_L;
+	case 'M': return ImGuiKey_M;
+	case 'N': return ImGuiKey_N;
+	case 'O': return ImGuiKey_O;
+	case 'P': return ImGuiKey_P;
+	case 'Q': return ImGuiKey_Q;
+	case 'R': return ImGuiKey_R;
+	case 'S': return ImGuiKey_S;
+	case 'T': return ImGuiKey_T;
+	case 'U': return ImGuiKey_U;
+	case 'V': return ImGuiKey_V;
+	case 'W': return ImGuiKey_W;
+	case 'X': return ImGuiKey_X;
+	case 'Y': return ImGuiKey_Y;
+	case 'Z': return ImGuiKey_Z;
+	case VK_F1: return ImGuiKey_F1;
+	case VK_F2: return ImGuiKey_F2;
+	case VK_F3: return ImGuiKey_F3;
+	case VK_F4: return ImGuiKey_F4;
+	case VK_F5: return ImGuiKey_F5;
+	case VK_F6: return ImGuiKey_F6;
+	case VK_F7: return ImGuiKey_F7;
+	case VK_F8: return ImGuiKey_F8;
+	case VK_F9: return ImGuiKey_F9;
+	case VK_F10: return ImGuiKey_F10;
+	case VK_F11: return ImGuiKey_F11;
+	case VK_F12: return ImGuiKey_F12;
+	case VK_F13: return ImGuiKey_F13;
+	case VK_F14: return ImGuiKey_F14;
+	case VK_F15: return ImGuiKey_F15;
+	case VK_F16: return ImGuiKey_F16;
+	case VK_F17: return ImGuiKey_F17;
+	case VK_F18: return ImGuiKey_F18;
+	case VK_F19: return ImGuiKey_F19;
+	case VK_F20: return ImGuiKey_F20;
+	case VK_F21: return ImGuiKey_F21;
+	case VK_F22: return ImGuiKey_F22;
+	case VK_F23: return ImGuiKey_F23;
+	case VK_F24: return ImGuiKey_F24;
+	case VK_BROWSER_BACK: return ImGuiKey_AppBack;
+	case VK_BROWSER_FORWARD: return ImGuiKey_AppForward;
+	default: return ImGuiKey_None;
+	}
+}
+
 auto Hook::KeyUpdate(__int64 key, int isdown)->void* {
 
 	// 防止在ImGui输入框中触发热键
@@ -789,27 +915,29 @@ auto Hook::KeyUpdate(__int64 key, int isdown)->void* {
 	//IMGUI 按键信号传递
 	if (ImGui::GetCurrentContext() != nullptr) {
 		ImGuiIO& io = ImGui::GetIO();
-		//io.KeysDown[(int)key] = isdown == 1;
 
-		io.KeyCtrl = (::GetKeyState(VK_CONTROL) & 0x8000) != 0;
-		io.KeyShift = (::GetKeyState(VK_SHIFT) & 0x8000) != 0;
-		io.KeyAlt = (::GetKeyState(VK_MENU) & 0x8000) != 0;
-		io.KeySuper = ((::GetKeyState(VK_LWIN) | ::GetKeyState(VK_RWIN)) & 0x8000) != 0;
+		ImGuiKey imKey = ImGui_VirtualKeyToImGuiKey(key);
+		if (imKey != ImGuiKey_None) {
+			io.AddKeyEvent(imKey, isdown == 1);
+			io.SetKeyEventNativeData(imKey, key, -1);
+			io.KeysDown[imKey] = isdown == 1;
+		}
+		io.AddKeyEvent(ImGuiMod_Ctrl, KEY_DOWN(VK_CONTROL));
+		io.AddKeyEvent(ImGuiMod_Shift, KEY_DOWN(VK_SHIFT));
+		io.AddKeyEvent(ImGuiMod_Alt, KEY_DOWN(VK_MENU));
+		io.AddKeyEvent(ImGuiMod_Super, KEY_DOWN(VK_APPS));
 		if (io.WantTextInput) {
-			if (key == VK_BACK) {
-				io.AddKeyEvent(ImGuiKey_Backspace, isdown == 1);
-				return 0;
-			}
 			if (isdown == 1) {
 				//if (Game::Cinstance) Game::Cinstance->setSuspendInput(false);
 				BYTE kb[256];
 				if (GetKeyboardState(kb)) {
-					wchar_t ch[6] = {};
+					wchar_t ch[6] = {0};
 					int ret = ToUnicode(static_cast<UINT>(key), MapVirtualKey(static_cast<UINT>(key), MAPVK_VK_TO_VSC), kb, (LPWSTR)ch, 5, 0);
 					if (ret > 0) {
 						io.AddInputCharacterUTF16(ch[0]);
 						return 0;
 					}
+					
 				}
 			}
 			return 0;
