4f94445c3baa8b6ae830b4f16e38e48ff3a6ad66
muzili <2586850402@qq.com>
2023-11-09 20:09:39 +0800

    版本更新到 1.8.0

--- Mod/version.h
+++ Mod/version.h
@@ -1,8 +1,8 @@
 ﻿#pragma once
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 7
-#define VERSION_REVISION 4
+#define VERSION_MINOR 8
+#define VERSION_REVISION 0
 #define VERSION_BUILD 0
 
 #define PLUGIN_NAME "Mod"
