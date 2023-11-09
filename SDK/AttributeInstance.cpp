364fb5b33c8793cfe4a948bc11eb72bbfc92811a
muzili <2586850402@qq.com>
2023-11-09 19:35:18 +0800

    尝试修复 AttributeInstance::getCurrentValue

--- SDK/AttributeInstance.cpp
+++ SDK/AttributeInstance.cpp
@@ -1,6 +1,6 @@
 ﻿#include "AttributeInstance.h"
 
-int AttributeInstance::getCurrentValueoffset = 0;
+int AttributeInstance::getCurrentValueoffset = 33;
 
 auto AttributeInstance::getCurrentValue()->float {
 	return *((float*)((char*)this + getCurrentValueoffset));			//这个偏移是偏移字节
